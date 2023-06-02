56b9b01082e1625a8375bc0ea74503b70b45c255
Tom | Surface <sassor@tablebricks.com>
2023-06-02 19:55:41 +0200

    Edit Service

--- ERP.Core/Services/CategoryService.cs
+++ ERP.Core/Services/CategoryService.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DbController;
+using ERP.Core.Models;
 
 namespace ERP.Core.Services
 {
-    internal class CategoryService
+    public class CategoryService : IModelService<Category, int>
     {
+        public async Task CreateAsync(Category input, IDbController dbController, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            string sql = $@"INSERT INTO `categories`
+(
+`name`,
+`description`
+)
+VALUES
+(
+@NAME,
+@DESCRIPTION
+); {dbController.GetLastIdSql()}";
+
+            input.CategoryId = await dbController.GetFirstAsync<int>(sql, input.GetParameters(), cancellationToken);
+        }
+
+        public async Task DeleteAsync(Category input, IDbController dbController, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            string sql = "DELETE FROM `categories` WHERE `category_id` = @GATEGORY_ID";
+
+            await dbController.QueryAsync(sql, input.GetParameters(), cancellationToken);
+        }
+
+        public async Task<Category?> GetAsync(int categoryId, IDbController dbController, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            string sql = "SELECT * FROM `categories` WHERE `category_id` = @CATEGORY_ID";
+
+            Category? category = await dbController.GetFirstAsync<Category>(sql, new
+            {
+                CATEGORY_ID = categoryId,
+            }, cancellationToken);
+
+            return category;
+        }
+
+        public async Task UpdateAsync(Category input, IDbController dbController, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            string sql = @"UPDATE `categories` SET
+`name` = @NAME,
+`description` = @DESCRIPTION
+WHERE `category_id` = @CATEGORY_ID";
+
+            await dbController.QueryAsync(sql, input.GetParameters(), cancellationToken);
+        }
     }
 }
